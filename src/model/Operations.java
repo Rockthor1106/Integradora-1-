2fc144b9b00a89c295658583a0500bf35371033c
Jhan Carlos Carvajal Bastidas <Liceo.sur.andino2015@gmail.com>
2020-09-18 19:02:23 -0500

    Implemented, a method that calculates the transport price of the materials for each location

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -1,9 +1,7 @@
 package model;
 public class Operations{
-	private static final int black_work = 1300000;
-	private static final int white_work = 2600000;
-	private static final int paint = 980000; 
-	private static final int total_work = 4880000;
+	 private static final int total_work = 4880000;
+	 
 	//method to calculate the total material prices
 	public static int total(int[] material_prices){
 		int total=0;
@@ -28,6 +26,7 @@ public class Operations{
 	  }
 		return best_prices;
 	}
+	//method that defines which store the best prices belong to
 	public static String[] stores(int[]best_prices,int[]homecenter,int[]local_store,int[]hub_store,int amount){
 		String[] stores = new String[amount];
 		for(int i=0;i<amount;i++){
@@ -42,5 +41,38 @@ public class Operations{
 			}
 		}
 		return stores;
+	}
+	//method that defines the price of transport of materials
+	public static int transport (int total,String location) {
+		 int price_transport=0;
+		 if(location.equalsIgnoreCase("norte") && total < 80000){
+			   price_transport = 120000;
+		  }
+			else if(location.equalsIgnoreCase("norte") && total < 300000) {
+				 price_transport = 28000;
+			}
+			else if(location.equalsIgnoreCase("norte") && total >= 300000) {
+				price_transport = 0;
+			}
+			else if(location.equalsIgnoreCase("sur") && total < 80000){
+			   price_transport = 120000;
+		  }
+			else if(location.equalsIgnoreCase("sur") && total < 300000) {
+				 price_transport = 55000;
+			}
+			else if(location.equalsIgnoreCase("sur") && total >= 300000) {
+				price_transport = 0;
+			}
+			else if(location.equalsIgnoreCase("centro") && total < 80000){
+			   price_transport = 50000;
+		  }
+			else if(location.equalsIgnoreCase("centro") && total < 300000) {
+				 price_transport = 0;
+			}
+			else if(location.equalsIgnoreCase("centro") && total >= 300000) {
+				price_transport = 0;
+			}
+			
+			return price_transport;
 	}
 } 
--- src/ui/Integrative.java
+++ src/ui/Integrative.java
@@ -7,7 +7,8 @@ public class Integrative{
  public static void main(String[]args){
 	 Scanner sc = new Scanner(System.in);
 	 int amount=0;
-	 System.out.println("¿Cuantos materiales desea ingeresar?:");
+	 String location = "";
+	 System.out.println("¿Cuantos materiales desea ingresar?:");
 	 amount = sc.nextInt(); 
    //arrays
 
@@ -20,12 +21,13 @@ public class Integrative{
 
    //get data 
 
+
 	 for(int i=0;i<amount;i++){
 		 System.out.println("Ingrese el nombre del material: ");
 		 name_material[i] = sc.next();
 		 System.out.println("Ingrese la cantidad del material: ");
 		 amount_material[i] = sc.nextDouble();
-		 System.out.println("Por favor indice la utilizacion del anterior material, presione: 1 para obra negra, 2 para obra blanca, y 3 para pintura: ");
+		 System.out.println("Por favor indique la utilizacion del anterior material, presione: 1 para obra negra, 2 para obra blanca, y 3 para pintura: ");
 		 utilization[i]=sc.nextInt();
 		 System.out.print("Ingrese el precio del producto en Home Center: ");
 		 homecenter[i]=sc.nextInt();
@@ -34,21 +36,25 @@ public class Integrative{
 		 System.out.print("Ingrese el precio del producto en la Ferreteria del Centro: ");
 		 hub_store[i]=sc.nextInt();
 	  }
-
+	 System.out.println("Ingrese por favor la ubicacion de su inmmueble, escriba norte, sur o centro");
+	 location = sc.next();
 	 //call methods 
 		
-		showData(homecenter,local_store,hub_store,name_material,amount);
+	 showData(homecenter,local_store,hub_store,name_material,amount,location);
+		
   }
 	
-	public static void showData(int[]homecenter,int[]local_store,int[]hub_store,String[]name_material,int amount){
-	 System.out.println("Total cuenta en Home Center incluyendo mano de obra: " + Operations.total(homecenter));
-	 System.out.println("Total cuenta en la Ferreteria del Barrio incluyendo mano de obra: " + Operations.total(local_store));
-	 System.out.println("Total cuenta en la Ferreteria del Centro incluyendo mano de obra:" + Operations.total(hub_store));
+	public static void showData(int[]homecenter,int[]local_store,int[]hub_store,String[]name_material,int amount,String location){
+	 System.out.println("Total cuenta en Home Center incluida mano de obra: " + (Operations.total(homecenter)));
+	 System.out.println("Total cuenta en la Ferreteria del Barrio incluia mano de obra: " + Operations.total(local_store));
+	 System.out.println("Total cuenta en la Ferreteria del Centro incluida mano de obra:" + Operations.total(hub_store));
 	 System.out.println("Las mejores opciones para comprar son: ");
 	 int[]best_prices= Operations.bestOption(homecenter,local_store,hub_store,amount);
 	 for(int i=0;i<amount;i++){	   
 		   System.out.println(name_material[i]+" a un precio de "+ Operations.bestOption(homecenter,local_store,hub_store,amount)[i]+" pesos en "+ Operations.stores(best_prices,homecenter,local_store,hub_store,amount)[i]);
 		}
-		System.out.println(Operations.total(best_prices));
+	 System.out.println("EL total de la cuenta con los mejores precios para cada producto incuida mano de obra es: " + Operations.total(best_prices) + " pesos");
+	 int total = Operations.total(best_prices);
+	 System.out.println("El precio de transporte es: " + Operations.transport(total,location) + " pesos");
   }
 }
