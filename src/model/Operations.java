20951287148cca3e5a0f55d1d814255e0f103324
Jhan Carlos Carvajal Bastidas <Liceo.sur.andino2015@gmail.com>
2020-09-19 22:07:09 -0500

    Implemented, javadoc

--- src/model/Operations.java
+++ src/model/Operations.java
@@ -1,8 +1,14 @@
 package model;
 public class Operations{
-	 private static final int total_work = 4880000;
 	 
-	//method to calculate the total material prices
+  /**<b> Method to calculate the total material prices </b><br>
+	*<b> pre: </b> The array matererial_prices must be declared and initialized <br>
+	*<b> pre: </b> The array material_prices have to be an array of integers <br>
+	*<b> pre: </b> The integers inside of array have to be the price of each material <br>
+	*<b> post: </b> The variable total must be declared and initialized <br>
+	*<b> post: </b> returns an integer with the total price of materials <br>
+	*@param material_prices An array of integers <br>
+	*/
 	public static int total(int[] material_prices){
 		int total=0;
 		for(int i=0;i<material_prices.length;i++){
@@ -10,7 +16,17 @@ public class Operations{
 		}
 		return total;
 	}
-	//method to calculate the best material prices
+	/**<b> Method to calculate the best material prices </b><br>
+	*<b> pre: </b> The arrays received per parameter must be declared and initialized <br>
+	*<b> pre: </b> Every array have to be of integers <br>
+	*<b> pre: </b> The integers inside of every array have to be the price of materials for each store<br>
+	*<b> post: </b> The array best_prices must be declared and initialized <br>
+	*<b> post: </b> returns an array of integers with the best prices of materials <br>
+	*@param homecenter An array of integers <br>
+	*@param local_store An array of integers <br>
+	*@param hub_store An array of integers <br>
+	*@param amount An integer <br>
+	*/
 	public static int[] bestOption(int[]homecenter,int[]local_store,int[]hub_store,int amount){
 		int[] best_prices=new int[amount];
 	  for(int i=0;i<amount;i++) {
@@ -26,7 +42,18 @@ public class Operations{
 	  }
 		return best_prices;
 	}
-	//method that defines which store the best prices belong to
+	/**<b> Method that defines which store the best prices belong to </b><br>
+	*<b> pre: </b> The arrays received per parameter must be declared and initialized <br>
+	*<b> pre: </b> Every array have to be of integers <br>
+	*<b> pre: </b> The integers inside of every array have to be the price of materials for each store<br>
+	*<b> post: </b> The array best_prices must be declared and initialized <br>
+	*<b> post: </b> returns an array of integers with the best prices of materials <br>
+	*@param best_prices An array of integers <br>
+	*@param homecenter An array of integers <br>
+	*@param local_store An array of integers <br>
+	*@param hub_store An array of integers <br>
+	*@param amount An integer <br>
+	*/
 	public static String[] stores(int[]best_prices,int[]homecenter,int[]local_store,int[]hub_store,int amount){
 		String[] stores = new String[amount];
 		for(int i=0;i<amount;i++){
@@ -42,7 +69,15 @@ public class Operations{
 		}
 		return stores;
 	}
-	//method that defines the price of transport of materials
+	/**<b> Method that defines the price of transport of materials for each ubication depending on the total of the account </b><br>
+	*<b> pre: </b> The arrays received per parameter must be declared and initialized <br>
+	*<b> pre: </b> The integer total have to be the total price of materials without labor <br>
+	*<b> pre: </b> The array location have to be only the words requested from the user by a message of console <br>
+	*<b> post: </b> The variable price_transport must be declared and initialized <br>
+	*<b> post: </b> returns an integer with the price of transport of materials<br>
+	*@param total An integer <br>
+	*@param location An array of string <br>
+	*/
 	public static int transport (int total,String location) {
 		 int price_transport=0;
 		 if(location.equalsIgnoreCase("norte") && total < 80000){
@@ -73,63 +108,44 @@ public class Operations{
 				price_transport = 0;
 			}
 			else {
-				 System.out.println("Error, por favor seleccione una ubicacion correcta");
+				 System.out.println("Error, por favor ingrese una ubicacion correcta");
 			}
 			return price_transport;
 	}
-	public static int countOnes (int[]utilization) {
-		 int countOnes=0;
+	/**<b> Method that counts the numbers that belong to the user selection </b><br>
+	*<b> pre: </b> The arrays and variables received per parameter must be declared and initialized <br>
+	*<b> pre: </b> The integer selection have to be a number between 1 and 3 <br>
+	*<b> post: </b> Is necessary declare and initialize a variable count <br>
+	*<b> post: </b> Returns a integer with the amount of numbers that belong to user selection, example amount of numbers one in the array utilization <br>
+	*@param utilization An array of integers <br>
+	*@param selection An integer <br>
+	*/
+	public static int count (int[]utilization,int selection) {
+		 int count=0;
 		 for(int i=0;i<utilization.length;i++) {
 			   if(utilization[i]==1) {
-					   countOnes++;
+					   count++;
 				  }
 		  }
-			return countOnes;
+			return count;
 	}
-	public static int countTwos (int[]utilization) {
-		 int countTwos=0;
+	/**<b> Method that creates an array where save the materials that belong to selection of user2 </b><br>
+	*<b> pre: </b> The arrays and variables received per parameter must be declared and initialized <br>
+	*<b> pre: </b> Selection have to be a number between 1 and 3 <br>
+	*<b> post: </b> Is necessary declare an array of string type for save the names of materials that belong to user selection <br>
+	*<b> post: </b> Returns an array with the materials that belong to selection of user<br>
+	*@param count An integer <br>
+	*@param name_material An array of string <br>
+	*@param utilization An array of integers <br>
+	*@param selection An integer <br>
+	*/
+	public static String[] CategoryConstruction (int count,String[]name_material,int[]utilization,int selection) {
+		 String[] CategoryConstruction = new String[count];
 		 for(int i=0;i<utilization.length;i++) {
-			   if(utilization[i]==2) {
-					   countTwos++;
+			   if(utilization[i]==selection) {
+					   CategoryConstruction[i]=name_material[i];
 				  }
 		  }
-			return countTwos;
+			return CategoryConstruction;
 	}
-	public static int countThrees (int[]utilization) {
-		 int countThrees=0;
-		 for(int i=0;i<utilization.length;i++) {
-			   if(utilization[i]==3) {
-					   countThrees++;
-				  }
-		  }
-			return countThrees;
-	}
-	public static String[] roughConstruction (int count,String[]name_material,int[]utilization) {
-		 String[] roughConstruction = new String[count];
-		 for(int i=0;i<utilization.length;i++) {
-			   if(utilization[i]==1) {
-					   roughConstruction[i]=name_material[i];
-				  }
-		  }
-			return roughConstruction;
-	}
-	public static String[] finalConstruction (int count,String[]name_material,int[]utilization) {
-		 String[] finalConstruction = new String[count];
-		 for(int i=0;i<utilization.length;i++) {
-			   if(utilization[i]==1) {
-					   finalConstruction[i]=name_material[i];
-				  }
-		  }
-			return finalConstruction;
-	}
-	public static String[] paint (int count,String[]name_material,int[]utilization) {
-		 String[] paint = new String[count];
-		 for(int i=0;i<utilization.length;i++) {
-			   if(utilization[i]==1) {
-					   paint[i]=name_material[i];
-				  }
-		  }
-			return paint;
-	}
-
 } 
--- src/ui/Integrative.java
+++ src/ui/Integrative.java
@@ -2,14 +2,23 @@ package ui;
 import model.*;
 import java.util.Scanner;
 public class Integrative{
-//method main to fill the arrays 
+ /**<b> Method main to declare the arrays and fill them</b><br>
+ *<b> pre: </b> The methods used in this method must be created and have correct operation <br>
+ *<b> pre: </b> The scanner sc must be iniatialized <br>
+ *<b> pre: </b> The arrays name_material, amount_material, utililzation, homecenter, local_store  and hub_store must be  declared and initialilzed <br>
+ *<b> pre: </b> The variable amount have to be an integer <br>
+ *<b> pre: </b> The variable selection have to be an integer<br>
+ *<b> pre: </b> The variable location have to be an string <br>
+ *<b> pre: </b> The data entered in the name_material array must be only words <br>
+ *<b> pre: </b> The integer introduced in the array utilization have to be only 1,2 o 3 <br>
+ */
 
  public static void main(String[]args){
 	 Scanner sc = new Scanner(System.in);
 	 int amount=0;
 	 int selection=0;
 	 String location = "";
-	 System.out.println("¿Cuantos materiales desea ingresar?:");
+   System.out.println("¿Cuantos materiales desea ingresar?:");
 	 amount = sc.nextInt(); 
 	 
    //arrays
@@ -23,7 +32,7 @@ public class Integrative{
 
    //get data 
   
-	 for(int i=0;i<amount;i++){
+	 for(int i=0;i<3;i++){
 		 System.out.println("Ingrese el nombre del material: ");
 		 name_material[i] = sc.next();
 		 System.out.println("Ingrese la cantidad del material: ");
@@ -48,7 +57,21 @@ public class Integrative{
 	 categoryList(sc,name_material,utilization,selection);
 		
   }
-	
+  /**<b> Method that shows to the user the data about materials after the relevant calculations </b><br>
+  *<b> pre: </b> The arrays received per parameter must be declared and initialized <br>
+  *<b> pre: </b> For every array the data inside of they must be of corresponding type<br>
+	*<b> pre: </b> The array location have to be only the words requested from the user by a message of console <br>
+	*<b> post: </b> Is necessary declare and initialize a new array where save the best prices for after use them in other method<br>
+	*<b> post: </b> Is necessary declare and initialize a variable of integer type where save the total of the best prices for after use that total in other method<br>
+	*<b> post: </b> This method does not return anything<br>
+	*@param homecenter An array of integers <br>
+	*@param local_store An array of integers <br>
+	*@param hub_store An array of integers <br>
+	*@param name_material An array of string <br>
+	*@param amount An integer <br>
+	*@param location A string <br>
+	*@param utilization An array of integers <br>
+	*/
 	public static void showData(int[]homecenter,int[]local_store,int[]hub_store,String[]name_material,int amount,String location,int[] utilization){
 	 System.out.println("Total cuenta en Home Center incluida mano de obra: " + (Operations.total(homecenter)));
 	 System.out.println("Total cuenta en la Ferreteria del Barrio incluia mano de obra: " + Operations.total(local_store));
@@ -63,6 +86,17 @@ public class Integrative{
 	 System.out.println("El precio de transporte es: " + Operations.transport(total,location) + " pesos");
 	  
   }
+	/** <b>Method that deploy a list with the material for the caterogy that the user selects </b><br>
+	*<b> pre: </b> Scanner sc must be initialized<br>
+	*<b> pre: </b> All the array received per parameter must be declared and initialized wit the corresponding data type <br>
+	*<b> pre: </b> The integer selection must be a number between 1 and 3<br>
+	*<b> post: </b>This method does not return anything<br>
+	*<b> post: </b>Is necessary declare a<br>
+	*@param sc A scanner <br>
+	*@param name_material An array of string <br>
+	*@param utilization An array of integers <br>
+	*@param selection An integer <br>
+	*/
 	public static void categoryList (Scanner sc,String[]name_material,int[]utilization,int selection) {
 	 int countOnes=Operations.countOnes(utilization);
 	 int countTwos=Operations.countTwos(utilization);
